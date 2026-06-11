904988fe07da9c310248893131d8d6afbf8697b5
Teja <chagantinvsteja@gmail.com>
2026-06-11 15:46:08 +0530

    Increase profile image upload limit

--- server/server/index.js
+++ server/server/index.js
@@ -36,7 +36,11 @@ app.use(
     },
   }),
 )
-app.use(express.json({ limit: '1mb' }))
+app.use(express.json({ limit: '10mb' }))
+app.use(express.urlencoded({
+  extended: true,
+  limit: '10mb',
+}))
 
 app.get('/', (req, res) => {
   res.send('CareerForge AI Backend Running...')
