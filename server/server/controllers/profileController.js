845cdcf2aac8087d69d899c72fe73c89c090f9d6
Teja <chagantinvsteja@gmail.com>
2026-06-11 15:14:31 +0530

    Add profile save debugging

--- server/server/controllers/profileController.js
+++ server/server/controllers/profileController.js
@@ -21,38 +21,30 @@ const getProfile =
     }
   }
 
-const saveProfile =
-  async (req, res) => {
 
-    try {
+     const saveProfile = async (req, res) => {
+  try {
+    console.log("PROFILE SAVE:", req.body)
 
-      const profile =
-        await Profile.findOneAndUpdate(
-          {
-            userId:
-              req.user._id,
-          },
-          {
-            ...req.body,
-            userId:
-              req.user._id,
-          },
-          {
-            upsert: true,
-            new: true,
-          }
-        )
+    const profile = await Profile.findOneAndUpdate(
+      { userId: req.user._id },
+      {
+        ...req.body,
+        userId: req.user._id,
+      },
+      {
+        upsert: true,
+        new: true,
+      }
+    )
 
-      res.json(profile)
-
-    } catch (error) {
-
-      res.status(500).json({
-        message: error.message,
-      })
-
-    }
+    res.json(profile)
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    })
   }
+}
 
 module.exports = {
   getProfile,
